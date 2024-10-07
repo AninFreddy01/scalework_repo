f79caef6b614d3c36f13805c054c153add61a52e
AninFredd01 <aninfreddy01@gmail.com>
2024-10-07 21:41:33 +0000

    modify test unit

--- individual_projects/unittest_bankSystem.py
+++ individual_projects/unittest_bankSystem.py
@@ -11,7 +11,16 @@ class TestBankAccount(unittest.TestCase):
         #Test if get_balance method returns the correct balance.
         self.assertEqual(self.account.get_balance(), "Your balance is: $100")
 
+    def test_deposit_valid_amount(self):
+        #Test deposit method with a valid amount.
+        self.account.deposit(50)
+        self.assertEqual(self.account.get_balance(), "Your balance is: $150")
     
+    def test_withdraw_valid_amount(self):
+        #Test withdraw method with a valid amount.
+        self.account.withdraw(30)
+        self.assertEqual(self.account.get_balance(), "Your balance is: $70")
+
 if __name__ == "__main__":
     unittest.main()
 
